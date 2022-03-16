61211ced0b32bad53b97a2ddf865cf882dcc6002
mizhousoft <lsp_lishuiping@qq.com>
2022-03-16 14:03:46 +0800

    新增工具类

---
+++ commons-lang/src/main/java/com/mizhousoft/commons/lang/LocalDateUtils.java
@@ -0,0 +1,36 @@
+package com.mizhousoft.commons.lang;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * 工具类
+ *
+ * @version
+ */
+public abstract class LocalDateUtils
+{
+	public static String formatYmd(LocalDate localDate)
+	{
+		if (null == localDate)
+		{
+			return null;
+		}
+
+		String pattern = "yyyy-MM-dd";
+
+		return localDate.format(DateTimeFormatter.ofPattern(pattern));
+	}
+
+	public static LocalDate parse(String date)
+	{
+		if (null == date)
+		{
+			return null;
+		}
+
+		String pattern = "yyyy-MM-dd";
+		LocalDate parse = LocalDate.parse(date, DateTimeFormatter.ofPattern(pattern));
+		return parse;
+	}
+}
