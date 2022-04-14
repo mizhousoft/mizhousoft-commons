cbd136fc35f576bab1312dc7f2e57b53326643e1
mizhousoft <lsp_lishuiping@qq.com>
2022-04-14 10:31:19 +0800

    优化接口

--- commons-lang/src/main/java/com/mizhousoft/commons/lang/LocalDateUtils.java
+++ commons-lang/src/main/java/com/mizhousoft/commons/lang/LocalDateUtils.java
@@ -10,26 +10,35 @@ import java.time.format.DateTimeFormatter;
  */
 public abstract class LocalDateUtils
 {
+	public static final String DEFAULT_PATTERN = "yyyy-MM-dd";
+
 	public static String formatYmd(LocalDate localDate)
+	{
+		return formatYmd(localDate, DEFAULT_PATTERN);
+	}
+
+	public static String formatYmd(LocalDate localDate, String pattern)
 	{
 		if (null == localDate)
 		{
 			return null;
 		}
 
-		String pattern = "yyyy-MM-dd";
-
 		return localDate.format(DateTimeFormatter.ofPattern(pattern));
 	}
 
 	public static LocalDate parseYmd(String date)
+	{
+		return parseYmd(date, DEFAULT_PATTERN);
+	}
+
+	public static LocalDate parseYmd(String date, String pattern)
 	{
 		if (null == date)
 		{
 			return null;
 		}
 
-		String pattern = "yyyy-MM-dd";
 		LocalDate parse = LocalDate.parse(date, DateTimeFormatter.ofPattern(pattern));
 		return parse;
 	}
